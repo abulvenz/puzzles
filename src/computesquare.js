d8c1e9a9b0d53f47853610e4b86ec3bcb2d7d45d
Andreas Eismann <andreas@eismaenners.de>
2018-07-22 00:41:02 +0200

    At least something has been achieved.

--- src/computesquare.js
+++ src/computesquare.js
@@ -1,15 +1,24 @@
 import m from 'mithril';
 import _ from 'underscore';
 
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let results = [66, 13, 60, 3, 72, 30];
+let n = {
+    numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+};
+let results__ = [66, 13, 60, 3, 72, 30];
+let results = [6, 15, 24, 12, 15, 18];
 
 let times_ = (a, b) => a * b;
 let minus_ = (a, b) => a - b;
 let add___ = (a, b) => a + b;
+let div___ = (a, b) => a / b;
 
 let id = a => a;
 let union = (...a) => [...a];
+
+let peek = a => {
+    // console.log(a);
+    return a;
+}
 
 var range = (startInclusive, endExclusive) => {
     let result = [];
@@ -19,7 +28,7 @@ var range = (startInclusive, endExclusive) => {
     return result;
 };
 
-let fs = [
+let fs____ = [
     [times_, minus_],
     [times_, minus_],
     [times_, times_],
@@ -27,13 +36,23 @@ let fs = [
     [times_, times_],
     [times_, times_]
 ];
-
+let fs = [
+    [add___, add___],
+    [add___, add___],
+    [add___, add___],
+    [add___, add___],
+    [add___, add___],
+    [add___, add___]
+];
 let empty = '';
 let equals = '=';
 let times = 'x';
 let minus = '-';
 
-let createEquation = (f, r) => n => f[1](f[0](n[0], n[1]), n[2]) - r;
+let createEquation = (f, r) => n => {
+    //  console.log('create', f, r, n)
+    return f[1](f[0](n[0], n[1]), n[2]) - r
+};
 
 const split = (arr, n) => {
     let result = [];
@@ -52,34 +71,28 @@ const transpose = (...arrOArrs) => zipWith(union, ...arrOArrs);
 
 console.log(transpose([0, 1], [2, 3]))
 
-console.log('tr', transpose(...split(numbers, 3)));
-console.log('id', split(numbers, 3));
+//console.log('tr', transpose(...split(numbers, 3)));
+//console.log('id', split(numbers, 3));
 
-let equations_ = numbers => [union, transpose].map(tr => tr(numbers));
+// let equations_ = numbers => [union, transpose].map(tr => tr(numbers));
 
+let on = (fns, arr) => fns.map((f, idx) => {
+    //    console.log('on', f, arr[idx]);
+    return f(arr[idx]);
+});
 
-let equa = numbers => fs
-    .map((f, idx) => createEquation(f, numbers, results[idx]))
+let equations = ()=> fs
+    .map((f, idx) => createEquation(f, results[idx]))
 
 
 let splitter = numbers => [...split(numbers, 3), ...transpose(...split(numbers, 3))]
 
-console.log(equa(numbers));
+const test_ = numbers => on(equations(), splitter(numbers).map(peek)).map(peek).every(e => e === 0);
 
-let equations = [
-    numbers => fs[0][1](fs[0][0](numbers[0], numbers[1]), numbers[2]) - results[0],
-    numbers => fs[1][1](fs[1][0](numbers[3], numbers[4]), numbers[5]) - results[1],
-    numbers => fs[2][1](fs[2][0](numbers[6], numbers[7]), numbers[8]) - results[2],
-    numbers => fs[3][1](fs[3][0](numbers[0], numbers[3]), numbers[6]) - results[3],
-    numbers => fs[4][1](fs[4][0](numbers[1], numbers[4]), numbers[7]) - results[4],
-    numbers => fs[5][1](fs[5][0](numbers[2], numbers[5]), numbers[8]) - results[5]
-];
-
-const test_ = numbers =>
-    equations.every(equation => equation(numbers) === 0);
+//console.log(test_(equations)([1, 2, 3, 4, 5, 6, 7, 8, 9]));
 
 const test = numbers =>
-    equations.every(equation => equation(numbers) === 0);
+    equations().every(equation => equation(numbers) === 0);
 
 const permute = permutation => {
     var length = permutation.length,
@@ -105,56 +118,164 @@ const permute = permutation => {
     return result;
 }
 
-console.log(permute(numbers))
-console.log(numbers = permute(numbers).filter(test)[0]);
+//console.log(permute(numbers))
 
-let A = numbers[0];
-let B = numbers[1];
-let C = numbers[2];
-let D = numbers[3];
-let E = numbers[4];
-let F = numbers[5];
-let G = numbers[6];
-let H = numbers[7];
-let I = numbers[8];
+//console.log(numbers = permute(numbers).filter(test_)[0]);
 
-let op1 = times;
-let op2 = minus;
-let op3 = times;
-let op4 = minus;
-let op5 = times;
-let op6 = times;
-
-let ops1 = minus;
-let ops2 = times;
-let ops3 = times;
-let ops4 = times;
-let ops5 = times;
-let ops6 = times;
-
-let R1 = 66;
-let R2 = 13;
-let R3 = 60;
-let R4 = 3;
-let R5 = 72;
-let R6 = 30;
-
-
-let fields = [
-    [A, op1, B, op2, C, equals, R1],
-    [ops1, empty, ops2, empty, ops3, empty, empty],
-    [D, op3, E, op4, F, equals, R2],
-    [ops4, empty, ops5, empty, ops6, empty, empty],
-    [G, op5, H, op6, I, equals, R3],
-    [equals, empty, equals, empty, equals, empty, empty],
-    [R4, empty, R5, empty, R6, empty, empty]
+const functions = [{
+        text: '+',
+        f: add___
+    },
+    {
+        text: '-',
+        f: minus_
+    },
+    {
+        text: 'x',
+        f: times_
+    },
+    {
+        text: '/',
+        f: div___
+    }
 ];
+
+const findSolution = () => permute([1,2,3,4,5,6,7,8,9])
+    .filter(test_)
+    [0];
+
+
+const fn2str = (f) => functions.filter(fn => fn.f === f)[0].text;
 
 console.log(fields)
 
-let fac = n => n === 1 ? 1 : n * fac(n - 1)
+let getNumber = idx => n.numbers[idx];
 
-console.log(fac(9))
+let setNumbers = num => n.numbers = num;
+
+class NumberView {
+    view(vnode) {
+        console.log(vnode.attrs)
+        return m('span', getNumber(vnode.attrs.idx));
+    }
+}
+
+class ResultInput {
+    view(vnode) {
+        return m('input[type=number,max=720]', {
+            oninput: e => console.log(results[vnode.attrs.idx] =  Number( e.target.value)),
+            value: results[vnode.attrs.idx],
+            style: 'width:30px'
+        });
+    }
+}
+
+class FunctionChooser {
+    view(vnode) {
+        return m('select', {
+            onchange: (e) => fs[vnode.attrs.eq][vnode.attrs.idx] = functions.filter(f => e.target.value === f.text)[0].f,
+            value: functions.filter(f => f.f === fs[vnode.attrs.eq][vnode.attrs.idx])[0].text
+        }, functions.map(f => m('option', f.text)))
+    }
+}
+
+let fields = [
+    [
+        m(NumberView, {
+            idx: 0,
+            n: n.numbers
+        }),
+        m(FunctionChooser, {
+            eq: 0,
+            idx: 0
+        }), m(NumberView, {
+            idx: 1
+        }),
+        m(FunctionChooser, {
+            eq: 0,
+            idx: 1
+        }), m(NumberView, {
+            idx: 2
+        }), '=', m(ResultInput, {
+            idx: 0
+        })
+    ],
+    [
+        m(FunctionChooser, {
+            eq: 3,
+            idx: 0
+        }), empty, m(FunctionChooser, {
+            eq: 4,
+            idx: 0
+        }), empty, m(FunctionChooser, {
+            eq: 5,
+            idx: 0
+        }), empty, empty
+    ],
+    [m(NumberView, {
+            idx: 3
+        }),
+        m(FunctionChooser, {
+            eq: 1,
+            idx: 0
+        }), m(NumberView, {
+            idx: 4
+        }),
+        m(FunctionChooser, {
+            eq: 1,
+            idx: 1
+        }), m(NumberView, {
+            idx: 5
+        }), equals, m(ResultInput, {
+            idx: 1
+        })
+    ],
+    [
+        m(FunctionChooser, {
+            eq: 3,
+            idx: 1
+        }), empty,
+        m(FunctionChooser, {
+            eq: 4,
+            idx: 1
+        }), empty,
+        m(FunctionChooser, {
+            eq: 5,
+            idx: 1
+        }), empty, empty
+    ],
+    [m(NumberView, {
+        idx: 6
+    }), m(FunctionChooser, {
+        eq: 2,
+        idx: 0
+    }), m(NumberView, {
+        idx: 7
+    }), m(FunctionChooser, {
+        eq: 2,
+        idx: 1
+    }), m(NumberView, {
+        idx: 8
+    }), equals, m(ResultInput, {
+        idx: 2
+    })],
+    [equals, empty, equals, empty, equals, empty, empty],
+    [m(ResultInput, {
+        idx: 3
+    }), empty, m(ResultInput, {
+        idx: 4
+    }), empty, m(ResultInput, {
+        idx: 5
+    }), empty, m('button', {
+        onclick: () => {
+            console.log(setNumbers(
+                _.flatten(permute([1, 2, 3, 4, 5, 6, 7, 8, 9]).filter(test_)[0])
+                //     [1,2,3,4,5,6,7,8,9]
+            ));
+            m.redraw()
+        }
+    }, 'go')]
+];
 
 export default class ComputeSquare {
     view(vnode) {
@@ -170,7 +291,13 @@ export default class ComputeSquare {
                     )
 
                 )
-            )
+
+            ),
+            m('pre', JSON.stringify(results)),
+            m('pre', JSON.stringify(n.numbers)),
+            m('pre', JSON.stringify(_.flatten(fs).map(fn2str)))
+
+            //     m('pre', JSON.stringify(numbers))
         );
     }
 };
